e6dcc0e0c4f5610e31c9e87bb900be1b2f0df741
Clóvis Balreira Rodrigues <62964663+clovisbalreira@users.noreply.github.com>
2023-08-18 02:33:20 -0300

    projetos python

---
+++ sites/boracodar/desafio32/app/api/completion/route.ts
@@ -0,0 +1,42 @@
+import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { Configuration, OpenAIApi } from 'openai-edge'
+
+// Create an OpenAI API client (that's edge friendly!)
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+})
+const openai = new OpenAIApi(config)
+ 
+// IMPORTANT! Set the runtime to edge
+export const runtime = 'edge'
+ 
+export async function POST(req: Request) {
+  // Extract the `messages` from the body of the request
+  const { schema, prompt } = await req.json()
+
+  const message = `
+    O seu trabalho é criar queries em SQL a partir de um schema SQL abaixo.
+    Schema SQL:
+    ***
+    ${schema}
+    ***
+
+    A partir do schema, escreve uma query SQL a partir da solitação abaixo:
+    Me Retorne somente o código SQL. nada alem disso
+
+    Solicitação: ${prompt}
+    `.trim()
+ 
+  // Ask OpenAI for a streaming chat completion given the prompt
+  const response = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    stream: true,
+    messages: [
+        {role: 'user', content: message}
+    ]
+  })
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response)
+  // Respond with the stream
+  return new StreamingTextResponse(stream)
+}
