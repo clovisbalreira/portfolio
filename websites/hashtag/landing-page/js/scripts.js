b4043e7226f27d43cee890f091764645036d2ca6
Clóvis Balreira Rodrigues <62964663+clovisbalreira@users.noreply.github.com>
2024-08-25 21:09:39 -0300

    colecao

---
+++ websites/hashtag/landing-page/js/scripts.js
@@ -0,0 +1,6 @@
+document.querySelectorAll("button").forEach(function (botao) {
+    botao.addEventListener("click", function () {
+      document.body.classList.toggle("popup--aberto");
+    });
+  });
+  
